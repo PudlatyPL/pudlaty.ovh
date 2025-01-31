35c9bed3969259a020530afe98c14fd6bd91872f
PudlatyPL <linojupi1@gmail.com>
2025-01-31 01:27:28 +0100

    Update project descriptions and improve meta description in index.html

--- src/ProjectsData.jsx
+++ src/ProjectsData.jsx
@@ -4,7 +4,12 @@ export const ProjectsData = [
         codeName: "wywrotkamcitemshop",
         name: "Minecraft ItemShop for WywrotkaMC",
         description: "Frontend and Backend of shop with dashboard",
-        fullDescription: (<p>Soon</p>),
+        fullDescription: (
+            <p>
+                This project involves creating a comprehensive item shop for the WywrotkaMC Minecraft server. 
+                It includes both frontend and backend development, with a user-friendly dashboard for managing items and transactions.
+            </p>
+        ),
         imgURL: "/1/prev.png",
         fullImgURL: "/1/full.png",
         tags: ["Python", "Flask", "MongoDB", "MySQL", "HTML", "CSS", "JavaScript"],
@@ -14,7 +19,12 @@ export const ProjectsData = [
         codeName: "hotmcitemshop",
         name: "Minecraft ItemShop for HotMC",
         description: "Frontend and Backend of shop with dashboard",
-        fullDescription: (<p>Soon</p>),
+        fullDescription: (
+            <p>
+                This project is a full-featured item shop for the HotMC Minecraft server. 
+                It features a robust backend and an intuitive frontend, along with a dashboard for easy management.
+            </p>
+        ),
         imgURL: "/2/prev.png",
         fullImgURL: "/2/full.png",
         tags: ["Python", "Flask", "MongoDB", "HTML", "CSS", "JavaScript"],
@@ -24,7 +34,12 @@ export const ProjectsData = [
         codeName: "dropcms",
         name: "Multi Domain Shop with CMS system",
         description: "Frontend and Backend of shop with dashboard",
-        fullDescription: (<p>Soon</p>),
+        fullDescription: (
+            <p>
+                This project is a multi-domain shop with an integrated CMS system. 
+                It allows for the management of multiple shops from a single dashboard, with a focus on scalability and ease of use.
+            </p>
+        ),
         imgURL: "/3/prev.png",
         fullImgURL: "/3/full.png",
         tags: ["Python", "Flask", "MongoDB", "HTML", "CSS", "JavaScript", "React", "Bootstrap"],
@@ -34,9 +49,14 @@ export const ProjectsData = [
         codeName: "portfolio",
         name: "Portfolio site for me",
         description: "Frontend of site",
-        fullDescription: (<p>Soon</p>),
+        fullDescription: (
+            <p>
+                This is a personal portfolio site showcasing my projects and skills. 
+                It features a clean and modern design, built with React and styled using TailwindCSS.
+            </p>
+        ),
         imgURL: "/4/prev.png",
-        fullImgURL: "/3/full.png",
+        fullImgURL: "/4/full.webp",
         tags: ["HTML", "CSS", "React", "TailwindCSS"],
     },
 ]
